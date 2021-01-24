b55de9236ab6dfc6a06769bda30d73c4b327a3d1
thbx456 <thbx456@gmail.com>
2021-01-23 21:04:32 -0500

    Plots and Scripts

--- Plot4.R
+++ Plot4.R
@@ -29,7 +29,7 @@ with(consumptiondata, {
 legend("topright", col = c("black", "red", "blue"), lty=1, lwd = 2, 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
         plot(Global_reactive_power ~ Datetime, type = "l",
-             ylab = "Global_Reactive_Power (kilowatts)", xlab = "")
+             ylab = "Global_Reactive_Power (kilowatts)", xlab = "Datetime")
 })
 
 dev.copy(png, file = "Plot4.png", height = 480, width = 480)
